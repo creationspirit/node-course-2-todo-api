0e31934f0755ee7ecb65ae1cd7aaec0a562b53be
Andrija <aperusic18@gmail.com>
2018-09-18 15:56:50 +0800

    Add update script

---
+++ playground/mongodb-update.js
@@ -0,0 +1,38 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
+  if (err) {
+    return console.log('Unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').findOneAndUpdate({
+  //   _id: new ObjectID('5ba044cc7f403208dad3fdc8')
+  // }, {
+  //   $set: {
+  //     completed: true5ba044cc7f403208dad3fdc8
+  //   }
+  // }, {
+  //   returnOriginal: false
+  // }).then((result) => {
+  //   console.log(result);
+  // });
+
+  db.collection('Users').findOneAndUpdate({
+    _id: new ObjectID('5ba044cc7f403208dad3fdc9')
+  }, {
+    $set: {
+      name: 'Andrija'
+    },
+    $inc: {
+      age: 1
+    }
+  }, {
+    returnOriginal: false
+  }).then((result) => {
+    console.log(result);
+  });
+  
+  //db.close();
+});
